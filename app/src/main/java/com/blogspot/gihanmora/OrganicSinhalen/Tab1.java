61ff8a99b4e89ad00338ca9282c096c7d5549d60
Gihanmora <gihangamage2015@gmail.com>
2019-03-09 02:03:23 +0530

    Creating Organic Sinhala Application

---
+++ app/src/main/java/com/blogspot/gihanmora/OrganicSinhalen/Tab1.java
@@ -0,0 +1,24 @@
+package com.blogspot.gihanmora.OrganicSinhalen;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Gihan bc on 8/18/2017.
+ */
+
+public class Tab1  extends Fragment  {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.tab1, container, false);
+
+
+        return rootView;
+    }
+
+}
